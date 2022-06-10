07a10b54b195863c3cec0d8ac65952aec395d058
KynZo <48533665+KynZo-Dev@users.noreply.github.com>
2022-06-10 12:54:16 +0200

    KynZo
    + add rememver_me to security
    + add form_themes bootstrap in twig
    + edit style.css
    + edit RegistrationFormType
    + edit base.html.twig
    + edit delete_form.html.twig
    + edit edit.html.twig
    + edit books\index.html.twig
    + edit books\new.html.twig
    + edit index.html.twig
    + edit register.html.twig
    + edit login.html.twig

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,7 +6,10 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,36 +22,48 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('LastName')
-            ->add('FirstName')
-            ->add('email')
-            ->add('Address')
-            ->add('PostalCode')
+            ->add('LastName', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('FirstName', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email'
+            ])
+            ->add('Address', TextType::class, [
+                'label' => 'Adresse'
+            ])
+            ->add('PostalCode', NumberType::class, [
+                'label' => 'Code Postal'
+            ])
             ->add('BirthDay', DateType::class, [
                 'widget' => 'single_text',
                 'attr' => ['class' => 'datepicker'],
                 'label' => 'Anniversaire'
             ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'Accepter conditions général',
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter nos conditions général.',
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => 'Mot de passe',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Enter un mot de passe valide',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),

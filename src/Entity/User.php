d494ac56e36b97884325d94dcf9c3a0b38443a27
KynZo <48533665+KynZo-Dev@users.noreply.github.com>
2022-06-10 19:13:39 +0200

    KynZo
    + add Reservations Entity
    + add Migrate
    + add ReservationRepository
    + edit User Entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -40,6 +42,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(type: 'date')]
     private $BirthDay;
+
+    #[ORM\ManyToMany(targetEntity: Reservations::class, mappedBy: 'User')]
+    private $reservations;
+
+    public function __construct()
+    {
+        $this->reservations = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -167,6 +177,33 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setBirthDay(\DateTimeInterface $BirthDay): self
     {
         $this->BirthDay = $BirthDay;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Reservations>
+     */
+    public function getReservations(): Collection
+    {
+        return $this->reservations;
+    }
+
+    public function addReservation(Reservations $reservation): self
+    {
+        if (!$this->reservations->contains($reservation)) {
+            $this->reservations[] = $reservation;
+            $reservation->addUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReservation(Reservations $reservation): self
+    {
+        if ($this->reservations->removeElement($reservation)) {
+            $reservation->removeUser($this);
+        }
 
         return $this;
     }
